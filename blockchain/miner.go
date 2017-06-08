5d395c946fcdb797ac4f77749881a2c264e7b600
David Julien <davidjulien@Davids-MacBook-Pro.local>
2017-06-07 23:00:04 -0700

    Clean up in miner.go

--- blockchain/miner.go
+++ blockchain/miner.go
@@ -3,18 +3,21 @@ package blockchain
 import (
 	"crypto/sha256"
 	"encoding/binary"
-	"log"
-	"strconv"
 	"time"
 )
 
 const (
 	// MiningHeaderLen is the length of the MiningHeader struct in bytes
 	MiningHeaderLen = (4 * (32 / 8)) + (2 * HashLen)
-	// CurrentVersion is the current version of the proof of work function
-	CurrentVersion = 1
+	// Version is the current version of the proof of work function
+	Version = 1
 	// MaxUint32 is the max value for the uint32 type
-	MaxUint32 = 4294967295
+	MaxUint32 uint32 = 4294967295
+)
+
+var (
+	// TargetHash is the target hash value of a mining header stored as type Hash
+	TargetHash Hash
 )
 
 // MiningHeader contains the metadata required for mining
@@ -22,9 +25,11 @@ type MiningHeader struct {
 	Version   uint32
 	LastBlock Hash
 	RootHash  Hash
-	Time      uint32
-	Target    uint32
-	Nonce     uint32
+	// Time is the current time as seconds since 1970-01-01T00:00 UTC
+	Time uint32
+	// Target is the current target stored in compact format
+	Target uint32
+	Nonce  uint32
 }
 
 // Marshal converts a Mining Header to a byte slice
@@ -41,45 +46,58 @@ func (mh *MiningHeader) Marshal() []byte {
 
 // SetMiningHeader sets the mining header
 func (mh *MiningHeader) SetMiningHeader(lastBlock Hash, rootHash Hash, target uint32) {
-	rawTimeString := time.Now().String()
-	formattedTimeString := rawTimeString[2:4] + rawTimeString[5:7] + rawTimeString[8:10] + rawTimeString[11:13] + rawTimeString[14:16]
-	formattedTime, e := strconv.ParseUint(formattedTimeString, 10, 32)
-
-	if e != nil {
-		log.Fatal(e)
-	}
-
-	time := uint32(formattedTime)
-
-	mh.Version = CurrentVersion
-	// TODO: have the block hashes be pulled from blockchain
+	mh.Version = Version
 	mh.LastBlock = lastBlock
 	mh.RootHash = rootHash
-	// TODO: have the current time be in unix time
-	mh.Time = uint32(time)
+	mh.Time = uint32(time.Now().Unix())
 	mh.Target = target
 	mh.Nonce = 0
 }
 
 // VerifyProofOfWork computes the hash of the MiningHeader and returns true if the result is less than the target
-func VerifyProofOfWork(mh *MiningHeader) bool {
-	hash := sha256.Sum256(mh.Marshal())
-	hash = sha256.Sum256(hash[:])
-
-	if CompareTo(hash, CompactToHash(mh.Target)) == LessThan {
+func (mh *MiningHeader) VerifyProofOfWork() bool {
+	if CompareTo(mh.DoubleHashSum(), TargetHash, LessThan) {
 		return true
 	}
 
 	return false
 }
 
+// DoubleHashSum computes the hash 256 of the marshalled mining header twice
+func (mh *MiningHeader) DoubleHashSum() Hash {
+	hash := sha256.Sum256(mh.Marshal())
+	hash = sha256.Sum256(hash[:])
+	return hash
+}
+
 // Mine continuously increases the nonce and tries to verify the proof of work until the puzzle is solved
-func Mine(mh *MiningHeader) bool {
-	for VerifyProofOfWork(mh) == false {
+func (mh *MiningHeader) Mine() bool {
+	if mh.VerifyMiningHeader() == false {
+		return false
+	}
+
+	TargetHash = CompactToHash(mh.Target)
+	for mh.VerifyProofOfWork() == false {
 		if mh.Nonce == MaxUint32 {
 			return false
 		}
 		mh.Nonce++
+	}
+	return true
+}
+
+// VerifyMiningHeader confirms that the mining header is properly set
+func (mh *MiningHeader) VerifyMiningHeader() bool {
+	if mh.Version != Version {
+		return false
+	}
+
+	if mh.Time == 0 || mh.Time > uint32(time.Now().Unix()) {
+		return false
+	}
+
+	if CompareTo(CompactToHash(mh.Target), MinHash, EqualTo) || CompareTo(CompactToHash(mh.Target), MaxDifficulty, GreaterThan) {
+		return false
 	}
 	return true
 }
@@ -89,16 +107,4 @@ func AppendUint32ToSlice(s *[]byte, num uint32) {
 	buf := make([]byte, 4)
 	binary.LittleEndian.PutUint32(buf, num)
 	*s = append(*s, buf...)
-}
-
-// BaseConverter converts a decimal number to a number of a specified base, in hex
-func BaseConverter(a uint32, b uint32) string {
-	if a < b {
-		s := strconv.FormatUint(uint64(a), 16)
-		if len(s) == 1 {
-			return "0" + s
-		}
-		return s
-	}
-	return BaseConverter(a/b, b) + strconv.FormatUint(uint64(a%b), 16)
 }
