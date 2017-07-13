4b6db99ef95c43ede78edea4cb5086061f660477
David Julien <juliendavid90@gmail.com>
2017-07-12 22:54:28 -0700

    TestBigAdd

--- common/math/big_test.go
+++ common/math/big_test.go
@@ -68,3 +68,40 @@ func TestBigSub(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestBigAdd(t *testing.T) {
+	a := big.NewInt(0)
+	b := BigAdd(big.NewInt(0), big.NewInt(0))
+
+	if a.Cmp(b) != 0 {
+		t.Fail()
+	}
+
+	a = big.NewInt(1)
+	b = BigAdd(big.NewInt(1), big.NewInt(0))
+
+	if a.Cmp(b) != 0 {
+		t.Fail()
+	}
+
+	a = big.NewInt(1)
+	b = BigAdd(big.NewInt(0), big.NewInt(1))
+
+	if a.Cmp(b) != 0 {
+		t.Fail()
+	}
+
+	a = big.NewInt(12)
+	b = BigAdd(big.NewInt(7), big.NewInt(5))
+
+	if a.Cmp(b) != 0 {
+		t.Fail()
+	}
+
+	a = big.NewInt(6)
+	b = BigAdd(big.NewInt(4), big.NewInt(2))
+
+	if a.Cmp(b) != 0 {
+		t.Fail()
+	}
+}
